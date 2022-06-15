b303905608f437fbed8cb8374b2fa72f2c4c9905
Goutte <antoine@goutenoir.com>
2022-06-15 05:04:20 +0200

    config: add the plugin release files

---
+++ addons/goutte.template.inja/plugin.gd
@@ -0,0 +1,12 @@
+tool
+extends EditorPlugin
+
+# The GdExtension is automatically loaded,
+# and it makes the Ginja object available already.
+# Perhaps we'll add some sugar nodes here later?
+
+func _enter_tree():
+	pass
+
+func _exit_tree():
+	pass
